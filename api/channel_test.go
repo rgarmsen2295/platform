a04d3ec006cd132d02cb0a44a57c3208fb12ed96
Reed Garmsen <rgarmsen2295@gmail.com>
2015-07-24 12:50:32 -0700

    Small change to error message explaining why it should now be failing

--- api/channel_test.go
+++ api/channel_test.go
@@ -663,7 +663,7 @@ func TestRemoveChannelMember(t *testing.T) {
 	Client.LoginByEmail(team.Name, userStd.Email, "pwd")
 
 	if _, err := Client.RemoveChannelMember(channel2.Id, userStd.Id); err == nil {
-		t.Fatal("Should have errored, user not channel admin")
+		t.Fatal("Should have errored, normal member cannot remove channel admin")
 	}
 
 	Client.LoginByEmail(team.Name, userTeamAdmin.Email, "pwd")
